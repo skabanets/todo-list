6a020a9911e057d29f13ad5749ab4ac9b243176a
Sergey Kabanets <skabanets97@gmail.com>
2024-04-02 15:34:38 +0300

    fix issue with filter

--- src/helpers/getFilteredTodos.js
+++ src/helpers/getFilteredTodos.js
@@ -2,7 +2,7 @@ export const getFilteredTodos = (todos, filter) => {
   switch (filter) {
     case 'Active':
       return todos.filter(item => item.complited === false);
-    case 'Complited':
+    case 'Completed':
       return todos.filter(item => item.complited === true);
     default:
       return todos;
