b63e3cca2c06501d3f4c79611acfb3ea8d557886
Sergey Kabanets <skabanets97@gmail.com>
2024-04-03 23:11:11 +0300

    add ts config

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -5,27 +5,24 @@ export const Filters = () => {
   const dispatch = useDispatch();
   const filter = useSelector(selectFilter);
 
-  const btnFilter =
-    'border-2 border-black px-4 py-2 rounded-md text-lg hover:text-white hover:bg-orange-400 w-[120px]';
-
   return (
     <div className="flex justify-center gap-2 my-5">
       <button
-        className={`${btnFilter} ${filter === 'All' ? 'bg-yellow-300' : ''}`}
+        className={`filterBtn ${filter === 'All' ? 'bg-yellow-300' : ''}`}
         type="button"
         onClick={() => dispatch(changeFilter('All'))}
       >
         All
       </button>
       <button
-        className={`${btnFilter} ${filter === 'Active' ? 'bg-yellow-300' : ''}`}
+        className={`filterBtn ${filter === 'Active' ? 'bg-yellow-300' : ''}`}
         type="button"
         onClick={() => dispatch(changeFilter('Active'))}
       >
         Active
       </button>
       <button
-        className={`${btnFilter} ${filter === 'Completed' ? 'bg-yellow-300' : ''}`}
+        className={`filterBtn ${filter === 'Completed' ? 'bg-yellow-300' : ''}`}
         type="button"
         onClick={() => dispatch(changeFilter('Completed'))}
       >
